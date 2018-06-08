7db940b520ec223d1be5552f2637eff6eafa2d5a
Jingrui Li <1079805974@qq.com>
2018-06-08 23:54:40 +0800

    f

---
+++ build/Thread.java
@@ -0,0 +1,15 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+package vm.lang;
+
+public class Thread implements Runnable {
+    public void run(){
+
+    }
+    public void start(){
+        start0();
+    }
+    public native void start0();
+}
