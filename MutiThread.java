ffe5463f715883ca5ca05b2bde27880a82d31eb6
Jingrui Li <1079805974@qq.com>
2018-07-01 17:38:02 +0800

    hehe

---
+++ MutiThread.java
@@ -0,0 +1,12 @@
+import java.lang.Thread;
+public class MutiThread{
+    public static void main(String[] args) {
+        for(int i=0;i<100;i++)
+        new Thread(){
+            @Override
+            public void run(){
+                System.out.println();
+            }
+        };
+    }
+}
